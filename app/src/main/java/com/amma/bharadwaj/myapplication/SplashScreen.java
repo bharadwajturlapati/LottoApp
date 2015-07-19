373022905ef59e58b6f8fc9839da9521de5e37ba
Bharadwaj Turlapati <tbj.name@gmail.com>
2015-07-19 19:43:14 +0530

    inital commit -1.0

---
+++ app/src/main/java/com/amma/bharadwaj/myapplication/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.amma.bharadwaj.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by bharadwaj on 7/6/2015.
+ */
+public class SplashScreen extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent(getBaseContext(),MainActivity.class);
+
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
+
